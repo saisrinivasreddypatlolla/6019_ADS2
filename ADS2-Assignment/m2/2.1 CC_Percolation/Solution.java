ef78e52ed156d9dcdc0681bf8928f2060052fe6a
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-10-30 19:34:15 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS2-Assignment/m2/2.1 CC_Percolation/Solution.java
+++ ADS2-Assignment/m2/2.1 CC_Percolation/Solution.java
@@ -1,47 +1,90 @@
-import java.util.*;
-class Solution {
-	static int v;
-	static boolean perculation(boolean[][] grid, Graph gr) {
-		for (int i = 0; i < v; i++) {
-			for (int j = 0; j < v; j++) {
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+class Perculation{
+	/**
+	 * number of vertices
+	 */
+	private int vertices;
+	/**
+	 * this method checks input is perculated or not
+	 * and returns the boolean value.
+	 * Time complexity is O(N^2).
+	 * addEdge method of graph method is called untill all
+	 * edges are connected.
+	 *
+	 * @param      grid  The grid
+	 * @param      graphObj    The graphics
+	 * @param      vertices1    The vertices
+	 *
+	 * @return     returns boolean value.
+	 */
+	public boolean perculation(final boolean[][] grid,
+		final Graph graphObj, final int vertices1) {
+		vertices = vertices1;
+		for (int i = 0; i < vertices; i++) {
+			for (int j = 0; j < vertices; j++) {
 				if (grid[i][j]) {
-					int tmp = trans(i, j);
+					int tmp = simplify(i, j);
 					if (i == 0) {
 
-						gr.addEdge(tmp, v * v);
+						graphObj.addEdge(tmp, vertices * vertices);
 					}
-					if (i == v - 1) {
-						gr.addEdge(tmp, v * v + 1);
+					if (i == vertices - 1) {
+						graphObj.addEdge(tmp, vertices * vertices + 1);
 					}
 					if (i - 1 >= 0 && grid[i - 1][j]) {
-						gr.addEdge(tmp, trans(i - 1, j));
+						graphObj.addEdge(tmp, simplify(i - 1, j));
 					}
-					if (i + 1 < v && grid[i + 1][j]) {
-						gr.addEdge(tmp, trans(i + 1, j));
+					if (i + 1 < vertices && grid[i + 1][j]) {
+						graphObj.addEdge(tmp, simplify(i + 1, j));
 					}
 					if (j - 1 >= 0 && grid[i][j - 1]) {
-						gr.addEdge(tmp, trans(i, j - 1));
+						graphObj.addEdge(tmp, simplify(i, j - 1));
 					}
-					if (j + 1 < v && grid[i][j + 1]) {
-						gr.addEdge(tmp, trans(i, j + 1));
+					if (j + 1 < vertices && grid[i][j + 1]) {
+						graphObj.addEdge(tmp, simplify(i, j + 1));
 					}
 
 				}
 			}
 		}
-		DepthFirstPaths dp = new DepthFirstPaths(gr, v * v);
-		return dp.hasPathTo(v * v + 1);
+		DepthFirstPaths dp = new DepthFirstPaths(graphObj, vertices * vertices);
+		return dp.hasPathTo(vertices * vertices + 1);
 	}
+	/**
+	 * this method gives the edge of graph.
+	 * Time complexity is O(1).
+	 *
+	 * @param      row     row
+	 * @param      column     column
+	 *
+	 * @return     returns the edge of the graph.
+	 */
+	public int simplify(int row, int column) {
+		return row * vertices + column;
+	}
+}
+class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution(){
 
-	static int trans(int i, int j) {
-		return i * v + j;
 	}
-	public static void main(String[] args) {
+	/**
+	 * this main method performs operations
+	 * Time complexity is O(N^2).
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
-		v = Integer.parseInt(scan.nextLine());
-		// v *= v;
-		boolean[][] grid = new boolean[v][v];
-		Graph g = new Graph(v * v + 2);
+		int vertices = Integer.parseInt(scan.nextLine());
+		Perculation obj = new Perculation();
+		boolean[][] grid = new boolean[vertices][vertices];
+		Graph gObj = new Graph(vertices * vertices + 2);
 		try {
 			while (scan.hasNext()) {
 				int row = scan.nextInt(), col = scan.nextInt();
@@ -51,7 +94,7 @@ class Solution {
 		} catch (Exception e) {
 
 		} finally {
-			System.out.println(perculation(grid, g));
+			System.out.println(obj.perculation(grid, gObj, vertices));
 		}
 	}
 }
