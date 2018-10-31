c31930bc18b26e1ff77bdc0b41da538b7fff422f
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-10-31 21:42:47 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS2-Assignment/m3/3.1 Directed Cycle/Bag.java
+++ ADS2-Assignment/m3/3.1 Directed Cycle/Bag.java
@@ -10,78 +10,131 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 /**
- *  The <tt>Bag</tt> class represents a bag (or multiset) of 
- *  generic items. It supports insertion and iterating over the 
+ *  The <tt>Bag</tt> class represents a bag (or multiset) of
+ *  generic items. It supports insertion and iterating over the
  *  items in arbitrary order.
  *  <p>
- *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em>  operation 
- *  take constant time. Iteration takes time proportional to the number of items.
+ *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em>  operation
+ *  take constant time. Iteration takes time
+ *  proportional to the number of items.
  *  <p>
- *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
+ *  For additional documentation, see
+ *  <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
  *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
  */
+/**
+ * Class for bag.
+ *
+ * @param      <Item>  The item
+ */
 public class Bag<Item> implements Iterable<Item> {
-    private int N;         // number of elements in bag
-    private Node first;    // beginning of bag
+    /**
+     * number of elements in bag
+     */
+    private int count;
+    /**
+     * beginning of bag
+     */
+    private Node first;
 
-    // helper linked list class
+    /**
+     * Class for node.
+     */
     private class Node {
+        /**
+         * element in the node
+         */
         private Item item;
+        /**
+         * next node address
+         */
         private Node next;
     }
 
-   /**
-     * Create an empty stack.
-     */
+    /**
+      * Create an empty stack.
+      */
     public Bag() {
         first = null;
-        N = 0;
+        count = 0;
     }
 
-   /**
-     * Is the BAG empty?
-     */
+    /**
+      * Is the BAG empty?
+      */
     public boolean isEmpty() {
         return first == null;
     }
 
-   /**
-     * Return the number of items in the bag.
-     */
+    /**
+      * Return the number of items in the bag.
+      */
     public int size() {
-        return N;
+        return count;
     }
 
-   /**
-     * Add the item to the bag.
+    /**
+     * this method adds node to the bag.
+     * Time complexity is O(1).
+     *
+     * @param      item  The item
      */
-    public void add(Item item) {
+    public void add(final Item item) {
         Node oldfirst = first;
         first = new Node();
         first.item = item;
         first.next = oldfirst;
-        N++;
+        count++;
     }
 
 
-   /**
-     * Return an iterator that iterates over the items in the bag.
+    /**
+     * Return an iterator that iterates over the
+     * items in the bag.
+     * Time complexity of this method is O(1)
+     * it returns elements in the bag.
+     *
+     *
+     * @return     Return an iterator that iterates
+     *             over the items in the bag.
      */
     public Iterator<Item> iterator()  {
-        return new ListIterator();  
+        return new ListIterator();
     }
-
-    // an iterator, doesn't implement remove() since it's optional
+    /**
+     * Class for list iterator.
+     */
     private class ListIterator implements Iterator<Item> {
+        /**
+         * this is the currnt node.
+         */
         private Node current = first;
-
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
-
+        /**
+         * Determines if it has next.
+         *
+         * @return     True if has next,
+         *             False otherwise.
+         */
+        public boolean hasNext() {
+            return current != null;
+        }
+        /**
+         * this method removes elements.
+         */
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+        /**
+         * this method will return the item of
+         * that current node.
+         * Time complexity is O(1).
+         *
+         * @return     returns the current node item.
+         */
         public Item next() {
             if (!hasNext()) throw new NoSuchElementException();
             Item item = current.item;
-            current = current.next; 
+            current = current.next;
             return item;
         }
     }
--- ADS2-Assignment/m3/3.1 Directed Cycle/Solution.java
+++ ADS2-Assignment/m3/3.1 Directed Cycle/Solution.java
@@ -1,98 +1,210 @@
 import java.util.Scanner;
-import java.util.*;
+/**
+ * Class for directed cycle.
+ */
 class DirectedCycle {
-	boolean[] marked;
-	boolean[] onStack;
-	int[] edgeTo;
-	Stack<Integer> cycle;
-	public DirectedCycle(DiGraph g) {
-		marked  = new boolean[g.V()];
-		onStack = new boolean[g.V()];
-		edgeTo  = new int[g.V()];
-		for (int v = 0; v < g.V(); v++) {
-			if (!marked[v] && cycle == null) {
-				dfs(g, v);
-			}
-		}
-	}
-	private void dfs(DiGraph g, int v) {
-		onStack[v] = true;
-		marked[v] = true;
-		for (int w : g.adj(v)) {
-			if (cycle != null) {
-				return;
-			}
-			else if (!marked[w]) {
-				edgeTo[w] = v;
-				dfs(g, w);
-			} else if (onStack[w]) {
-				cycle = new Stack<Integer>();
-				for (int x = v; x != w; x = edgeTo[x]) {
-					cycle.push(x);
-				}
-				cycle.push(w);
-				cycle.push(v);
-			}
-		}
-		onStack[v] = false;
+    /**
+     * boolean array for marking the visit of node.
+     */
+    boolean[] marked;
+    /**
+     * boolean array if the node is in the stack
+     */
+    boolean[] onStack;
+    /**
+     * array for the edge of that node.
+     */
+    int[] edgeTo;
+    /**
+     * stack object to store nodes.
+     */
+    Stack<Integer> cycle;
+    /**
+     * Constructs the object.
+     * Time complexity is O(V).
+     * V is the number of vertices.
+     *
+     * @param      g     diGraph object
+     */
+    public DirectedCycle(final DiGraph g) {
+        marked  = new boolean[g.vertices()];
+        onStack = new boolean[g.vertices()];
+        edgeTo  = new int[g.vertices()];
+        for (int vertices = 0; vertices < g.vertices(); vertices++) {
+            if (!marked[vertices] && cycle == null) {
+                dfs(g, vertices);
+            }
+        }
+    }
+    /**
+     * this method performs dfs for the given graph.
+     * Time complexity is O(E)
+     * E is the number of edges.
+     *
+     * @param      g     object of digraph
+     * @param      vertices     number of vertice.
+     */
+    private void dfs(final DiGraph g, final int vertices) {
+        onStack[vertices] = true;
+        marked[vertices] = true;
+        for (int w : g.adj(vertices)) {
+            if (cycle != null) {
+                return;
+            }
+            else if (!marked[w]) {
+                edgeTo[w] = vertices;
+                dfs(g, w);
+            } else if (onStack[w]) {
+                cycle = new Stack<Integer>();
+                for (int x = vertices; x != w; x = edgeTo[x]) {
+                    cycle.push(x);
+                }
+                cycle.push(w);
+                cycle.push(vertices);
+            }
+        }
+        onStack[vertices] = false;
 
-	}
-	public boolean hasCycle() {
-		return cycle != null;
-	}
+    }
+    /**
+     * Determines if it has cycle.
+     * Time complexity of this method is O(1)
+     *
+     * @return     True if has cycle, 
+     *             False otherwise.
+     */
+    public boolean hasCycle() {
+        return cycle != null;
+    }
 }
+/**
+ * Class for di graph.
+ */
 class DiGraph {
-	int v, e, edge = 0;
-	int indegree = 0;
-	Bag<Integer>[] adj;
-	DiGraph(Scanner scan) {
-		v = Integer.parseInt(scan.nextLine());
-		adj = (Bag<Integer>[]) new Bag[v];
-		for (int i = 0; i < v; i++) {
-			adj[i] = new Bag<Integer>();
-		}
-		e = Integer.parseInt(scan.nextLine());
-		for (int i = 0; i < e; i++) {
-			addEdge(scan.nextInt(), scan.nextInt());
-		}
-	}
-	public void addEdge(int v1, int v2) {
-		if (!hasEdge(v1, v2)) {
-			edge++;
-		}
-		adj[v2].add(v1);
-	}
-	public boolean hasEdge(final int vertexOne,
-	                       final int vertexTwo) {
-		for (int each : adj(vertexOne))  {
-			if (each == vertexTwo) {
-				return true;
-			}
-		}
-		return false;
-	}
-	public int V() {
-		return v;
-	}
-	public int E() {
-		return e;
-	}
-	public Iterable<Integer> adj(final int vertex) {
-		return adj[vertex];
-	}
+    /**
+     * number of vertices
+     */
+    private int vertices;
+    /**
+     * number of edges.
+     */
+    private int edges;
+    /**
+     * edge count
+     */
+    private int edge = 0;
+    /**
+     * Indegree of that node.
+     */
+    private int indegree = 0;
+    /**
+     * array to store that adjacent nodes.
+     */
+    private Bag<Integer>[] adj;
+    /**
+     * Constructs the object.
+     * Time complexity is O(V+E);
+     * V is number of vertices
+     * E is number of edges.
+     *
+     * @param      scan  The scan
+     */
+    DiGraph(final Scanner scan) {
+        vertices = Integer.parseInt(scan.nextLine());
+        adj = (Bag<Integer>[]) new Bag[vertices];
+        for (int i = 0; i < vertices; i++) {
+            adj[i] = new Bag<Integer>();
+        }
+        edges = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < edges; i++) {
+            addEdge(scan.nextInt(), scan.nextInt());
+        }
+    }
+    /**
+     * Adds an edge.
+     * Time complexity is O(1).
+     *
+     * @param      vertexOne    The vertexOne
+     * @param      vertexTwo    The vertexTwo
+     */
+    public void addEdge(final int vertexOne,
+        final int vertexTwo) {
+        if (!hasEdge(vertexOne, vertexTwo)) {
+            edge++;
+        }
+        adj[vertexTwo].add(vertexOne);
+    }
+    /**
+     * Determines if it has edge.
+     * Time complexity of this method is O(E)
+     * E is the number of edges.
+     *
+     * @param      vertexOne  The vertex one
+     * @param      vertexTwo  The vertex two
+     *
+     * @return     True if has edge, False otherwise.
+     */
+    public boolean hasEdge(final int vertexOne,
+                           final int vertexTwo) {
+        for (int each : adj(vertexOne))  {
+            if (each == vertexTwo) {
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * returns the number of vetices.
+     *
+     * @return     returns the number of vetices.
+     */
+    public int vertices() {
+        return vertices;
+    }
+    /**
+     * returns the number of edges.
+     *
+     * @return     returns the number of edges.
+     */
+    public int edges() {
+        return edges;
+    }
+    /**
+     * returns the adjacent vetices of that vertex.
+     *
+     * @param      vertex  The vertex
+     *
+     * @return     returns the adjacent vetices of that vertex.
+     */
+    public Iterable<Integer> adj(final int vertex) {
+        return adj[vertex];
+    }
 }
+/**
+ * Class for solution.
+ */
 final class Solution {
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		DiGraph g = new DiGraph(scan);
-		DirectedCycle obj = new DirectedCycle(g);
-		if(obj.hasCycle()){
-			System.out.println("Cycle exists.");
-		} else{
-			System.out.println("Cycle doesn't exists.");
-		}
-	}
+    }
+    /**
+     * main method performs the operation.
+     * Time complexity of this method is O(N).
+     * N is number of edges
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        DiGraph g = new DiGraph(scan);
+        DirectedCycle obj = new DirectedCycle(g);
+        if(obj.hasCycle()){
+            System.out.println("Cycle exists.");
+        } else{
+            System.out.println("Cycle doesn't exists.");
+        }
+    }
 }
