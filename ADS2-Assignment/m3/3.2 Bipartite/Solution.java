29f80fd07d4d4ec0ff206958a4124b32c5b2e0df
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-10-31 23:37:31 +0530

    "ads2_m3_a2_1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS2-Assignment/m3/3.2 Bipartite/Solution.java
+++ ADS2-Assignment/m3/3.2 Bipartite/Solution.java
@@ -1,41 +1,47 @@
 import java.util.Scanner;
-class Bipartite{
+/**
+ * Class for bipartite.
+ */
+class Bipartite {
+	/**
+	 * { var_description }
+	 */
 	boolean[] marked;
 	boolean[] color;
 	boolean flag;
 	int[] edgeTo;
-	Bipartite(Graph g){
+	Bipartite(Graph g) {
 		int vertices = g.vertices();
 		marked = new boolean[vertices];
 		color = new boolean[vertices];
 		edgeTo = new int[vertices];
 		flag = true;
-		for(int i =0;i<vertices;i = edgeTo[i]){
-			if(!marked[i]){
+		for (int i = 0; i < vertices; i = edgeTo[i]) {
+			if (!marked[i]) {
 				dfs(g, i);
 			}
 		}
 	}
-	public void dfs(Graph g, int vertices){
+	public void dfs(Graph g, int vertices) {
 		marked[vertices] = true;
 		color[vertices] = true;
 		for (int w : g.adj(vertices)) {
-            if (!flag) {
-                return;
-            } else if (!marked[w]) {
-                edgeTo[w] = vertices;
-                dfs(g, w);
-            } else if (color[w] == color[vertices]) {
-            	flag = false;
-            }
-        }
+			if (!flag) {
+				return;
+			} else if (!marked[w]) {
+				edgeTo[w] = vertices;
+				dfs(g, w);
+			} else if (color[w] == color[vertices]) {
+				flag = false;
+			}
+		}
 	}
-	public boolean isBipartite(){
+	public boolean isBipartite() {
 		return flag;
 	}
 }
-class Solution{
-	private Solution(){
+class Solution {
+	private Solution() {
 
 	}
 	public static void main(String[] args) {
@@ -43,10 +49,13 @@ class Solution{
 		int vertices = Integer.parseInt(scan.nextLine());
 		int edges = Integer.parseInt(scan.nextLine());
 		Graph g = new Graph(vertices);
+		for (int i = 0; i < edges; i++) {
+			g.addEdge(scan.nextInt(), scan.nextInt());
+		}
 		Bipartite obj = new Bipartite(g);
-		if(obj.isBipartite()){
+		if (obj.isBipartite()) {
 			System.out.println("Graph is bipartite");
-		} else{
+		} else {
 			System.out.println("Graph is not a bipartite");
 		}
 	}
