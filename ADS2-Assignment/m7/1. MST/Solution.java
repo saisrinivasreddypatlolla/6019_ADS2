f3bb09a608b5499abb1e329c96b2aeb0036860a3
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-11-05 20:11:01 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS2-Assignment/m7/1. MST/Solution.java
+++ ADS2-Assignment/m7/1. MST/Solution.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int vertices = Integer.parseInt(scan.nextLine());
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int vertices = Integer.parseInt(scan.nextLine());
         int edges = Integer.parseInt(scan.nextLine());
         Edge edgeObj;
         EdgeWeightedGraph graph
@@ -31,133 +31,138 @@ class Solution {
         MSTree mstObj = new MSTree(graph);
         double result = mstObj.total();
         System.out.format("%.5f", result);
-	}
+    }
 }
 /**
  * Class for edge.
  */
 class Edge implements Comparable<Edge> {
-	/**
-	 * { var_description }.
-	 */
-	private int vertexOne;
-	/**
-	 * { var_description }.
-	 */
-	private int vertexTwo;
-	/**
-	 * { var_description }.
-	 */
-	private double weight;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      v     { parameter_description }
-	 * @param      w     { parameter_description }
-	 * @param      wei   The wei
-	 */
-	Edge(final int v, final int w, final double wei) {
-		this.vertexOne = v;
-		this.vertexTwo = w;
-		this.weight = wei;
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public int either() {
-		return vertexOne;
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      v     { parameter_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public int other(final int v) {
-		if (vertexOne == v) {
-			return vertexTwo;
-		} return vertexOne;
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      that  The that
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public int compareTo(final Edge that) {
-		if (this.weight < that.weight) {
-			return -1;
-		} else if (this.weight > that.weight) {
-			return 1;
-		} return 0;
-	}
-	/**
-	 * Gets the weight.
-	 *
-	 * @return     The weight.
-	 */
-	public double getWeight(){
-		return this.weight;
-	}
+    /**
+     * { var_description }.
+     */
+    private int vertexOne;
+    /**
+     * { var_description }.
+     */
+    private int vertexTwo;
+    /**
+     * { var_description }.
+     */
+    private double weight;
+    /**
+     * Constructs the object.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     * @param      wei   The wei
+     */
+    Edge(final int v, final int w, final double wei) {
+        this.vertexOne = v;
+        this.vertexTwo = w;
+        this.weight = wei;
+    }
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int either() {
+        return vertexOne;
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int other(final int v) {
+        if (vertexOne == v) {
+            return vertexTwo;
+        }
+        return vertexOne;
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      that  The that
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int compareTo(final Edge that) {
+        if (this.weight < that.weight) {
+            return -1;
+        } else if (this.weight > that.weight) {
+            return 1;
+        }
+        return 0;
+    }
+    /**
+     * Gets the weight.
+     *
+     * @return     The weight.
+     */
+    public double getWeight(){
+        return this.weight;
+    }
 }
 /**
  * Class for edge weighted graph.
  */
 class EdgeWeightedGraph {
-	/**
-	 * .
-	 */
-	private int vertices;
-	private Bag<Edge>[] adj;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      v     { parameter_description }
-	 */
-	EdgeWeightedGraph(final int v) {
-		this.vertices = v;
-		adj = new Bag[vertices];
-		for (int i = 0; i < vertices; i++) {
-			adj[i] = new Bag<Edge>();
-		}
-	}
-	/**
-	 * Adds an edge.
-	 *
-	 * @param      e     { parameter_description }
-	 */
-	public void addEdge(final Edge e) {
-		int first = e.either();
-		int second = e.other(first);
-		adj[first].add(e);
-		adj[second].add(e);
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public Iterable<Edge> edges() {
-		Bag<Edge> list = new Bag<Edge>();
-		for (int i = 0; i < vertices; i++) {
-			for (Edge e : adj(i)) {
-				list.add(e);
-			}
-		}
-		return list;
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      vertex  The vertex
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public Iterable<Edge> adj(final int vertex) {
+    /**
+     * .
+     */
+    private int vertices;
+    /**
+     * .
+     */
+    private Bag<Edge>[] adj;
+    /**
+     * Constructs the object.
+     *
+     * @param      v     { parameter_description }
+     */
+    EdgeWeightedGraph(final int v) {
+        this.vertices = v;
+        adj = new Bag[vertices];
+        for (int i = 0; i < vertices; i++) {
+            adj[i] = new Bag<Edge>();
+        }
+    }
+    /**
+     * Adds an edge.
+     *
+     * @param      e     { parameter_description }
+     */
+    public void addEdge(final Edge e) {
+        int first = e.either();
+        int second = e.other(first);
+        adj[first].add(e);
+        adj[second].add(e);
+    }
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Iterable<Edge> edges() {
+        Bag<Edge> list = new Bag<Edge>();
+        for (int i = 0; i < vertices; i++) {
+            for (Edge e : adj(i)) {
+                list.add(e);
+            }
+        }
+        return list;
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      vertex  The vertex
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Iterable<Edge> adj(final int vertex) {
         return adj[vertex];
     }
     public int vertices() {
@@ -165,6 +170,9 @@ class EdgeWeightedGraph {
     }
 
 }
+/**
+ * Class for ms tree.
+ */
 class MSTree {
     /**
      *the graph object.
