c8d1cd29a4c5c5fbe5010ac3bfb25d7a9a721ba0
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-11-05 19:50:57 +0530

    "ads2_m7_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS2-Assignment/m7/1. MST/Solution.java
@@ -0,0 +1,128 @@
+import java.util.Scanner;
+class Solution {
+	private Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+	}
+}
+class Edge implements Comparable<Edge> {
+	int vertexOne;
+	int vertexTwo;
+	double weight;
+	Edge(int v, int w, double wei) {
+		this.vertexOne = v;
+		this.vertexTwo = w;
+		this.weight = wei;
+	}
+	public int either() {
+		return vertexOne;
+	}
+	public int other(int v) {
+		if (vertexOne == v) {
+			return vertexTwo;
+		} return vertexOne;
+	}
+	public int compareTo(Edge that) {
+		if (this.weight < that.weight) {
+			return -1;
+		} else if (this.weight > that.weight) {
+			return 1;
+		} return 0;
+	}
+	public double getWeight(){
+		return this.weight;
+	}
+}
+class EdgeWeightedGraph {
+	int vertices;
+	Bag<Edge>[] adj;
+	EdgeWeightedGraph(int v) {
+		this.vertices = v;
+		adj = new Bag[vertices];
+		for (int i = 0; i < vertices; i++) {
+			adj[i] = new Bag<Edge>();
+		}
+	}
+	public void addEdge(Edge e) {
+		int first = e.either();
+		int second = e.other(first);
+		adj[first].add(e);
+		adj[second].add(e);
+	}
+	public Iterable<Edge> edges() {
+		Bag<Edge> list = new Bag<Edge>();
+		for (int i = 0; i < vertices; i++) {
+			for (Edge e : adj(i)) {
+				list.add(e);
+			}
+		}
+		return list;
+	}
+	public Iterable<Edge> adj(final int vertex) {
+        return adj[vertex];
+    }
+    public int vertices() {
+        return this.vertices;
+    }
+
+}
+class MSTree {
+    /**
+     *the graph object.
+     */
+    private EdgeWeightedGraph graph;
+    /**
+     *queue to give the mst.
+     */
+    private Queue<Edge> mst;
+    /**
+     *intializes  the values.
+     *time complexity is O(ElogE).
+     *where e is the edges.
+     * @param      g  graph object.
+     */
+    MSTree(final EdgeWeightedGraph g) {
+        graph = g;
+        mst = new Queue<Edge>();
+        MinPQ<Edge> pq = new MinPQ<Edge>();
+        for (Edge edge : graph.edges()) {
+            pq.insert(edge);
+        }
+        UF ufObj = new UF(graph.vertices());
+        while (!pq.isEmpty()
+                && mst.size() < graph.vertices() - 1) {
+            Edge edge = pq.delMin();
+            int vertexOne = edge.either();
+            int vertexTwo = edge.other(vertexOne);
+            if (!ufObj.connected(vertexOne, vertexTwo)) {
+                ufObj.union(vertexOne, vertexTwo);
+                mst.enqueue(edge);
+            }
+        }
+    }
+    /**
+     *it returns all the edges on mst.
+     *
+     * @return  queue which contains all
+     * vertices.
+     */
+    public Iterable<Edge> edges() {
+        return mst;
+    }
+    /**
+     *this method returns the total weight.
+     *of mst.
+     *time complexity is O(E)
+     * @return weight of mst.
+     */
+    public double total() {
+        double sum = 0.0;
+        for (Edge e : edges()) {
+            sum += e.getWeight();
+        }
+        return sum;
+    }
+}
