9239825181bf0914cc7d577f318ab028dc2714cf
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-11-10 11:52:13 +0530

    "ads2_m12_a1_1: 5/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS2-Assignment/m12/ADS-2_Exam-2/Solution.java
+++ ADS2-Assignment/m12/ADS-2_Exam-2/Solution.java
@@ -18,10 +18,6 @@ public class Solution {
 			break;
 
 		case "DirectedPaths":
-			// Handle the case of DirectedPaths, where two integers are given.
-			// First is the source and second is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
 			String[] path = scan.nextLine().split(" ");
 			DijkstraSP obj = new DijkstraSP(graph, Integer.parseInt(path[0]));
 			if(obj.hasPathTo(Integer.parseInt(path[1]))){
@@ -32,11 +28,6 @@ public class Solution {
 			break;
 
 		case "ViaPaths":
-			// Handle the case of ViaPaths, where three integers are given.
-			// First is the source and second is the via is the one where path should pass throuh.
-			// third is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
 			String[] viaPath = scan.nextLine().split(" ");
 			DijkstraSP obj1 = new DijkstraSP(graph, Integer.parseInt(viaPath[0]));
 			DijkstraSP obj2 = new DijkstraSP(graph, Integer.parseInt(viaPath[1]));
