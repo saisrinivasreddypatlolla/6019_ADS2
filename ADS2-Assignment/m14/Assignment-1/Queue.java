3c10a4f94e629fac54a2f112e311d743e3e8c56e
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-11-18 21:44:50 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS2-Assignment/m14/Assignment-1/Queue.java
@@ -0,0 +1,177 @@
+import java.util.Iterator;
+/**
+ * List of Queue.
+ *
+ * @param      <Item>  The item
+ */
+public class Queue<Item> implements Iterable<Item> {
+    /**
+     * beginning of queue.
+     */
+    private Node<Item> first;
+    /**
+     * end of queue.
+     */
+    private Node<Item> last;
+    /**
+     * number of elements on queue.
+     */
+    private int n;
+
+
+    /**
+     * Class for node.
+     *
+     * @param      <Item>  The item
+     */
+    private static class Node<Item> {
+        /**
+         * Item.
+         */
+        private Item item;
+        /**
+         * next of type node.
+         */
+        private Node<Item> next;
+    }
+
+    /**
+     * Initializes an empty queue.
+     */
+    Queue() {
+        first = null;
+        last  = null;
+        n = 0;
+    }
+
+    /**
+     * Returns true if this queue is empty.
+     * complexity O(1)
+     * @return {@code true} if this queue is empty.
+     * {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    /**
+     * Returns the number of items in this queue.
+     * complexity O(1)
+     * @return the number of items in this queue
+     */
+    public int size() {
+        return n;
+    }
+
+    /**
+     * Returns the item least recently added to this queue.
+     * complexity O(1)
+     * @return the item least recently added to this queue
+     */
+    public Item peek() {
+        return first.item;
+    }
+
+    /**
+     * Adds the item to this queue.
+     * complexity O(1)
+     * @param  item the item to add
+     */
+    public void enqueue(final Item item) {
+        Node<Item> oldlast = last;
+        last = new Node<Item>();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) {
+            first = last;
+        } else {
+            oldlast.next = last;
+        }
+        n++;
+    }
+
+    /**
+     * Removes and returns the item on this queue that was least recently added.
+     * complexity O(1)
+     * @return the item on this queue that was least recently added
+     */
+    public Item dequeue() {
+        Item item = first.item;
+        first = first.next;
+        n--;
+        if (isEmpty()) {
+            last = null;
+        }
+        return item;
+    }
+
+    /**
+     * Returns a string representation of this queue.
+     * complexity O(1)
+     * @return the sequence of items in FIFO order, separated by spaces
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Item item : this) {
+            s.append(item);
+            s.append("\n");
+        }
+        return s.toString();
+    }
+
+    /**
+     *
+     * @return an iterator that iterates over the items
+     * in this queue in FIFO order
+     */
+    public Iterator<Item> iterator()  {
+        return new ListIterator<Item>(first);
+    }
+
+    /**
+     * Class for list iterator.
+     *
+     * @param      <Item>  The item
+     */
+    private class ListIterator<Item> implements Iterator<Item> {
+        /**
+         * Current Node.
+         */
+        private Node<Item> current;
+        /**
+         * Constructs the object.
+         *
+         * @param      first1  The first
+         */
+        ListIterator(final Node<Item> first1) {
+            current = first1;
+        }
+
+        /**
+         * Determines if it has next.
+        * complexity O(1)
+         * @return     True if has next, False otherwise.
+         */
+        public boolean hasNext() {
+            return current != null;
+        }
+        /**
+         * Method to remove.
+        * complexity O(1)
+         */
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        /**
+         * Method for finding the next.
+         *complexity O(1)
+         * @return     Item
+         */
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+}
---
+++ ADS2-Assignment/m14/Assignment-1/Solution.java
@@ -0,0 +1,229 @@
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+
+	}
+	/**
+	 * main method to peform operations.
+	 * complexity O(M*N)
+	 * M number of words in file
+	 * N word length.
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
+		String[] words = loadWords();
+		Scanner scan = new Scanner(System.in);
+		String token = scan.nextLine();
+		TST tst = new TST();
+		for (String each : words) {
+			for (int i = 0; i < each.length(); i++) {
+				tst.put(each.substring(i, each.length()), 0);
+			}
+		}
+		System.out.println(tst.keysWithPrefix(token));
+	}
+	/**
+	 * Loads words.
+	 * @return     array.
+	 */
+	public static String[] loadWords() {
+		In in = new In("/Files/dictionary-algs4.txt");
+		String[] words = in.readAllStrings();
+		return words;
+	}
+}
+/**
+ * Class for tst.
+ *
+ * @param      <Value>  The value
+ */
+class TST<Value> {
+	/**
+	 * int variable.
+	 */
+	private int n;
+	/**
+	 * Node object.
+	 */
+	private Node<Value> root;
+	/**
+	 * Class for node.
+	 *
+	 * @param      <Value>  The value
+	 */
+	class Node<Value> {
+		/**
+		 * char variable.
+		 */
+		private char character;
+		/**
+		 * node object.
+		 */
+		private Node<Value> left;
+		/**
+		 * node object.
+		 */
+		private Node<Value> middle;
+		/**
+		 * node object.
+		 */
+		private Node<Value> right;
+		/**
+		 * value variable.
+		 */
+		private Value value;
+	}
+	/**
+	 * Constructs the object.
+	 */
+	TST() {
+
+	}
+	/**
+	 * checks if the string is present or not.
+	 * complexity is O(L + logN)
+	 * L length of string, N trie size.
+	 * because we are calling the get method.
+	 * @param      one   One
+	 *
+	 * @return     boolean value.
+	 */
+	public boolean contains(final String one) {
+		return get(one) != null;
+	}
+	/**
+	 * helper method for the main get method.
+	 * complexity is O(L + logN)
+	 * L length of string, N trie size.
+	 * @param      one   One
+	 *
+	 * @return     the string.
+	 */
+	public Value get(final String one) {
+		Node<Value> node = get(root, one, 0);
+		if (node == null) {
+			return null;
+		}
+		return node.value;
+	}
+	/**
+	 * it returns the value.
+	 * complexity is O(L + logN)
+	 * L length of string, N trie size.
+	 * @param      node  The node
+	 * @param      one   One
+	 * @param      d     the character
+	 *
+	 * @return     node object.
+	 */
+	public Node<Value> get(final Node<Value> node,
+	                       final String one, final int d) {
+		if (node == null) {
+			return null;
+		}
+		char ch = one.charAt(d);
+		if (ch < node.character) {
+			return get(node.left,  one, d);
+		} else if (ch > node.character) {
+			return get(node.right, one, d);
+		} else if (d < one.length() - 1) {
+			return get(node.middle, one, d + 1);
+		} else {
+			return node;
+		}
+	}
+	/**
+	 * helper method for the main put method.
+	 * complexity is O(L + logN)
+	 * L length of string, N trie size.
+	 * @param      one    One
+	 * @param      value  The value
+	 */
+	public void put(final String one, final Value value) {
+		if (!contains(one)) {
+			n++;
+		}
+		root = put(root, one, value, 0);
+	}
+	/**
+	 * used to put the string with the assigned value.
+	 * complexity is O(L + logN)
+	 * L length of string, N trie size.
+	 * @param      node   The node
+	 * @param      one    One
+	 * @param      value  The value
+	 * @param      d      the charcter index.
+	 *
+	 * @return     Node object.
+	 */
+	public Node<Value> put(final Node<Value> node,
+	                       final String one, final Value value,
+	                       final int d) {
+		Node node1 = node;
+		char ch = one.charAt(d);
+		if (node1 == null) {
+			node1 = new Node<Value>();
+			node1.character = ch;
+		}
+		if (ch < node1.character) {
+			node1.left  = put(node1.left,  one, value, d);
+		} else if (ch > node1.character) {
+			node1.right = put(node1.right, one, value, d);
+		} else if (d < one.length() - 1) {
+			node1.middle   = put(node1.middle, one, value, d + 1);
+		} else {
+			node1.value   = value;
+		}
+		return node1;
+	}
+	/**
+	 * used to find the string with the prefix.
+	 * complexity is O(L + logN)
+	 * L length of string, N trie size.
+	 * @param      one   One
+	 *
+	 * @return    Iterable.
+	 */
+	public Iterable<String> keysWithPrefix(final String one) {
+		Queue<String> que = new Queue<String>();
+		Node<Value> x = get(root, one, 0);
+		if (x == null) {
+			return que;
+		}
+		if (x.value != null) {
+			que.enqueue(one);
+		}
+		collect(x.middle, new StringBuilder(one), que);
+		return que;
+	}
+	/**
+	 * checks for the strings with the preffix
+	 * in trie.
+	 * complexity is O(L + logN)
+	 * L length of string, N trie size.
+	 * @param      node  The node
+	 * @param      one   One
+	 * @param      que   The que
+	 */
+	public void collect(final Node<Value> node,
+	                    final StringBuilder one, final Queue<String> que) {
+		if (node == null) {
+			return;
+		}
+		collect(node.left,  one, que);
+		if (node.value != null) {
+			que.enqueue(one.toString()
+			            + node.character);
+		}
+		collect(node.middle, one.append(
+		            node.character), que);
+		one.deleteCharAt(one.length() - 1);
+		collect(node.right, one, que);
+	}
+}
